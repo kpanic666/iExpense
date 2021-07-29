3482e7da7a35672838ef8577e63bd09dbbd3d22e
Andrei Korikov <kpanic666@gmail.com>
2021-07-29 23:52:27 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Andrei Korikov on 29.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
